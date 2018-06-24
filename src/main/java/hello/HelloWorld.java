14d6514393b298256aadcecbd9d9c1d3a087d5e1
abdoulaye <soabdoulaye91@gmail.com>
2018-06-25 00:15:54 +0200

    added java src

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The Current Time " + currentTime);
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+		
+	}
+
+}
